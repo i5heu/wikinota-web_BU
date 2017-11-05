e17be1cb58af9a54de0eaa634bf5ae7e84f269a5
i5heu <i5heu@i5h.eu>
2017-11-05 11:18:44 +0100

    test

--- js/main.js
+++ js/main.js
@@ -40,3 +40,65 @@ var ModulFunctions = new Vue({
     this.LoginChek()
   }
 })
+
+
+var desk = new Vue({
+
+  el: '#desk',
+
+  data: {
+  },
+  created: function () {
+    console.log("test");
+
+
+  // POST /someUrl
+  this.$http.post('http://localhost:8080/api', {foo: 'bar'}).then(response => {
+
+    // get status
+    response.status;
+
+    // get status text
+    response.statusText;
+
+    // get 'Expires' header
+    response.headers.get('Expires');
+
+    // get body data
+    this.someData = response.body;
+
+  }, response => {
+    // error callback
+  });
+
+
+
+  },
+
+  watch: {
+
+  },
+  methods: {
+
+  }
+})
+
+
+
+var DeskPortal = new Vue({
+
+  el: '#DeskPortal',
+
+  data: {
+  },
+  created: function () {
+    console.log("test2");
+  },
+
+  watch: {
+
+  },
+  methods: {
+
+  }
+})
